ecb9abb92eb4a41096094b2b329a5f032c5a8a40
theburntcrumpet <msksansom@gmail.com>
2019-05-30 18:23:12 +0100

    Enemy Animation etc

--- Assets/Scripts/Enemy/Enemy Battle/FollowPlayerX.cs
+++ Assets/Scripts/Enemy/Enemy Battle/FollowPlayerX.cs
@@ -8,17 +8,35 @@ public class FollowPlayerX : MonoBehaviour
     public float stopDistance;
     public Transform target;
 
+    SpriteRenderer render;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        render = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) > stopDistance){
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y),speed * Time.deltaTime);
+        float distance = Vector2.Distance(transform.position, target.position);
+        if (distance > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+            anim.SetBool("Run", true);
+            anim.SetBool("Idle", false);
         }
+        else if (distance <= stopDistance)
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Run", false);
+        }
+        if (target.position.x > transform.position.x)
+            render.flipX = true;
+        else if (target.position.x < transform.position.x)
+            render.flipX = false;
     }
 }
